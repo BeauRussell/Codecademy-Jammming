e5fa3750cb69c5880726b0f76c4de2797a0f7c36
Beau Russell <beaur97@att.net>
2018-02-04 14:33:54 -0600

    Fixed Adding Tracks
    Fixed the syntax of the method call to allow the user program to add the songs to the playlist when the user adds the playlist to their account

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -70,14 +70,14 @@ const Spotify = {
 
 		addTracks(playlistID, trackURIs, userId) {
 			console.log(trackURIs);
-			fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistID}/tracks`), {
+			fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistID}/tracks`, {
 				method: 'POST',
 				headers: {
 					Authorization: `Bearer ${accessToken}`,
 					"Content-Type": 'application/json'
 				},
 				body: JSON.stringify({uris: trackURIs})
-			}
+			});
 		},
 
 		findUserId() {
